7b2dcf83739a4a4fc0347b8bb3f46090a90fa446
rahul <rahulv881@gmail.com>
2022-02-19 22:50:16 +0530

    Added user info mock data

---
+++ lib/model/user_info_mock_data.dart
@@ -0,0 +1,15 @@
+class UserInfoMockData {
+  String name;
+  String userId;
+  String profileImgUrl;
+  String password;
+  String mobileNumber;
+
+  UserInfoMockData({
+    required this.name,
+    required this.userId,
+    required this.profileImgUrl,
+    required this.password,
+    required this.mobileNumber,
+  });
+}
